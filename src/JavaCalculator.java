406e52756c11fe2a90dd233a3e9095a4f7e7d8c1
Bernardino Villagra Báez <bernardinovillagra@hotmail.com>
2023-10-27 16:23:52 -0300

    Calculadora funcional

--- src/JavaCalculator.java
+++ src/JavaCalculator.java
@@ -1,2 +1,188 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class JavaCalculator {
+    private Integer total1 = 0;
+    private Integer total2 = 0;
+    private char math_operator;
+    private boolean display = false;
+    private JPanel JavaCalculator;
+    private JTextField textDisplay;
+    private JButton btnOne;
+    private JButton btnMultiply;
+    private JButton btnTwo;
+    private JButton btnThree;
+    private JButton btnFour;
+    private JButton btnFive;
+    private JButton btnSix;
+    private JButton btnDivide;
+    private JButton btnSeven;
+    private JButton btnEight;
+    private JButton btnNine;
+    private JButton btnPlus;
+    private JButton btnZero;
+    private JButton btnClear;
+    private JButton btnEquals;
+    private JButton btnMinus;
+
+    private void clearTextDisplay(){
+        if ( display ) {
+            textDisplay.setText("");
+            display = false;
+        }
+    }
+    private void getOperator(String btnText) {
+        math_operator = btnText.charAt(0);
+        total1 = total1 + Integer.parseInt(textDisplay.getText());
+        textDisplay.setText("");
+    }
+    public JavaCalculator() {
+        btnOne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnOneText = textDisplay.getText() + btnOne.getText();
+                textDisplay.setText(btnOneText);
+            }
+        });
+        btnTwo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnTwoText = textDisplay.getText() + btnTwo.getText();
+                textDisplay.setText(btnTwoText);
+            }
+        });
+        btnThree.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnThreeText = textDisplay.getText() + btnThree.getText();
+                textDisplay.setText(btnThreeText);
+            }
+        });
+        btnFour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnFourText = textDisplay.getText() + btnFour.getText();
+                textDisplay.setText(btnFourText);
+            }
+        });
+        btnFive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnFiveText = textDisplay.getText() + btnFive.getText();
+                textDisplay.setText(btnFiveText);
+            }
+        });
+        btnSix.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnSixText = textDisplay.getText() + btnSix.getText();
+                textDisplay.setText(btnSixText);
+            }
+        });
+        btnSeven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnSevenText = textDisplay.getText() + btnSeven.getText();
+                textDisplay.setText(btnSevenText);
+            }
+        });
+        btnEight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnEightText = textDisplay.getText() + btnEight.getText();
+                textDisplay.setText(btnEightText);
+            }
+        });
+        btnNine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnNineText = textDisplay.getText() + btnNine.getText();
+                textDisplay.setText(btnNineText);
+            }
+        });
+        btnZero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearTextDisplay();
+                String btnZeroText = textDisplay.getText() + btnZero.getText();
+                textDisplay.setText(btnZeroText);
+            }
+        });
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textDisplay.setText("");
+                total2 = 0;
+            }
+        });
+        btnPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String button_text = btnPlus.getText();
+                getOperator(button_text);
+            }
+        });
+        btnEquals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (math_operator) {
+                    case '+':
+                        total2 = total1 + Integer.parseInt(textDisplay.getText());
+                        break;
+                    case '-':
+                        total2 = total1 - Integer.parseInt(textDisplay.getText());
+                        break;
+                    case '*':
+                        total2 = total1 * Integer.parseInt(textDisplay.getText());
+                        break;
+                    case '/':
+                        total2 = total1 / Integer.parseInt(textDisplay.getText());
+                        break;
+                }
+
+                textDisplay.setText(Integer.toString(total2));
+                total1 = 0;
+                display = true;
+            }
+        });
+        btnMultiply.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String button_text = btnMultiply.getText();
+                getOperator(button_text);
+            }
+        });
+        btnDivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String button_text = btnDivide.getText();
+                getOperator(button_text);
+            }
+        });
+        btnMinus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String button_text = btnMinus.getText();
+                getOperator(button_text);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculadora");
+        frame.setContentPane(new JavaCalculator().JavaCalculator);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
